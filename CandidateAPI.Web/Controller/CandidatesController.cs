64e0791d173ee3de51e878a30ffe75cb7f3267ea
bouhtich abdelhamid <abdelhamid.bouhtich@terabyte-software.com>
2024-06-13 12:59:38 +0100

    add controller

---
+++ CandidateAPI.Web/Controller/CandidatesController.cs
@@ -0,0 +1,31 @@
+﻿using CandidateAPI.Application.Services;
+using CandidateAPI.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandidateAPI.Web.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CandidatesController : ControllerBase
+    {
+        private readonly CandidateService _candidateService;
+        public CandidatesController(CandidateService candidateService)
+        {
+            _candidateService = candidateService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrUpdateCandidate([FromBody] Candidate candidate)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result= await _candidateService.AddOrUpdateCandidateAsync(candidate);
+            return Ok(result);
+        }
+
+
+    }
+}
